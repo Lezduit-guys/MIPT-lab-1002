8f47f8d45f105fe765a4287869adb2e6cdfb68d8
Lezduit-guys <infokasparas@gmail.com>
2024-10-02 22:21:01 +0300

    paspaudus mygtuka, pasikeicia tekstas

--- app/src/main/java/com/example/miptlab10918/MainActivity.java
+++ app/src/main/java/com/example/miptlab10918/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.miptlab10918;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +11,25 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    TextView tvMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        this.tvMain = findViewById(R.id.tvMain);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.Main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
     }
+
+    public void onBtnChangeTextClick(View view){
+        this.tvMain.setText("LEzduuiit");
+    }
+
 }
